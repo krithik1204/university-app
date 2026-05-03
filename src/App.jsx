2d1bcf32cbd57f472b577bfa9db9799c7d1ff6fd
krithik1204 <mukkeraraju1235@gmail.com>
2026-05-03 21:49:25 +0530

    implement resource server connetivity and fetch user profile data in dashboard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,7 +8,7 @@ function App() {
     <div>
       <div>
            <h1 class="text-3xl font-bold underline">
-    Hello world!
+    Hello world!.........
   </h1>
       </div>
     </div>
